a6b766a971871c4fa1744e10b849cd39816fc3d1
Jordy Kristianata <jordykristianata@gmail>
2026-03-24 13:47:31 +0700

    fixing cors issue

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // all important modules
-const process=require('process');
-const express=require('express');
-const app=express();
-const cors=require('cors');
+const process = require('process');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
 // const path = require("path");
 // const bodyParser = require('body-parser');
@@ -44,12 +44,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Headers", "*");
     return next();
 });
 
 
 // include router
-const apiRouter=require("./routes/apiRouter");
+const apiRouter = require("./routes/apiRouter");
 app.use("/api", apiRouter);
 
 // start server at localhost
@@ -59,4 +60,4 @@ app.use("/api", apiRouter);
 
 // start server with serverless
 
-module.exports=app;
+module.exports = app;
